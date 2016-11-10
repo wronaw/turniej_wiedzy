02e1aca2540d6c382291e08a009e461f55a5477b
Wiktor <business.wrona@gmail.com>
2016-11-10 17:12:20 +0100

    Pierwsze pliki

---
+++ turniej_wiedzy.py
@@ -0,0 +1,85 @@
+# Turniej wiedzy
+# Gra sprawdzająca wiedzę ogólną, odczytująca dane ze zwykłego pliku tekstowego
+
+import sys
+
+def open_file(file_name, mode):
+    """Otwórz plik."""
+    try:
+        the_file = open(file_name, mode)
+    except IOError as e:
+        print("Nie można otworzyć pliku", file_name, "Program zostanie zakończony.\n", e)
+        input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
+        sys.exit()
+    else:
+        return the_file
+
+def next_line(the_file):
+    """Zwróć kolejny wiersz pliku kwiz po sformatowaniu go."""
+    line = the_file.readline()
+    line = line.replace("/", "\n")
+    return line
+
+def next_block(the_file):
+    """Zwróć kolejny blok danych z pliku kwiz."""
+    category = next_line(the_file)
+    
+    question = next_line(the_file)
+    
+    answers = []
+    for i in range(4):
+        answers.append(next_line(the_file))
+        
+    correct = next_line(the_file)
+    if correct:
+        correct = correct[0]
+        
+    explanation = next_line(the_file)
+
+    plusscore = next_line(the_file) 
+
+    return category, question, answers, correct, explanation, plusscore
+
+def welcome(title):
+    """Przywitaj gracza i pobierz jego nazwę."""
+    print("\t\t Witaj w turnieju wiedzy!\n")
+    print("\t\t", title, "\n")
+ 
+def main():
+    trivia_file = open_file("kwiz.txt", "r")
+    title = next_line(trivia_file)
+    welcome(title)
+    score = 0
+
+    # pobierz pierwszy blok
+    category, question, answers, correct, explanation, plusscore = next_block(trivia_file)
+    while plusscore:
+        # zadaj pytanie
+        print(category)
+        print(question)
+        print("Za to pytanie dostaniesz", plusscore, "punktów")
+        for i in range(4):
+            print("\t", i + 1, "-", answers[i])
+
+        # uzyskaj odpowiedź
+        answer = input("Jaka jest Twoja odpowiedź?: ")
+
+        # sprawdź odpowiedź
+        if answer == correct:
+            print("\nOdpowiedź prawidłowa!", end=" ")
+            score += int(plusscore)
+        else:
+            print("\nOdpowiedź niepoprawna.", end=" ")
+        print(explanation)
+        print("Wynik:", score, "\n\n")
+
+        # pobierz kolejny blok
+        category, question, answers, correct, explanation, plusscore = next_block(trivia_file)
+
+    trivia_file.close()
+
+    print("To było ostatnie pytanie!")
+    print("Twój końcowy wynik wynosi", score)
+ 
+main()  
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
